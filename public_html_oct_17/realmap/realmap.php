ab692ffe4c5a6a7da1850f55e3b5a72b435fd78b
hrishikeshk <kv_hrishikesh@yahoo.com>
2019-03-06 19:00:44 +0530

    map sizing change and display of address text fields

--- public_html_oct_17/realmap/realmap.php
+++ public_html_oct_17/realmap/realmap.php
@@ -1,17 +1,17 @@
 <?php
   $lat_lng = array();
   $address = '';
+  $city = '';
+  $zip = '';
   if(isset($_POST['address']) && strlen($_POST['address']) > 0){
     $address = $_POST['address'];
     $lat_lng = get_lat_lng($address);
   }
-  $city = '';
-  if(isset($_POST['city']) && strlen($_POST['city']) > 0){
+  else if(isset($_POST['city']) && strlen($_POST['city']) > 0){
     $city = $_POST['city'];
     $lat_lng = get_lat_lng($city);
   }
-  $zip = '';
-  if(isset($_POST['zip']) && strlen($_POST['zip']) > 0){
+  else if(isset($_POST['zip']) && strlen($_POST['zip']) > 0){
     $zip = $_POST['zip'];
     $lat_lng = get_lat_lng($zip);
   }
@@ -29,7 +29,7 @@
   function get_lat_lng($loc){
     $url = "https://maps.googleapis.com/maps/api/geocode/json?key=AIzaSyATdAW-nZvscm35rSLI8Bu9eGq84odzVLA&address=".urlencode(trim($loc))."&sensor=false";
 	  $result_string = file_get_contents_curl($url);
-	  error_log($result_string);
+	  ////error_log($result_string);
     $result = json_decode($result_string, true);
     $lat = $result['results'][0]['geometry']['location']['lat'];
     $lng = $result['results'][0]['geometry']['location']['lng'];
@@ -45,7 +45,8 @@
     <meta charset="utf-8">
     <style>
       #map {
-        height: 100%;
+        height: 80%;
+        width: 80%;
       }
       html, body {
         height: 100%;
@@ -58,44 +59,96 @@
   <form action="realmap.php" method="post">
     <table>
     <tr>
-      <td>Address: <input type="text" id="address" name="address" /><br /> -- OR -- <br /></td>
-    </tr>
-    <tr>
-      <td>City: <input type="text" id="city" name="city" /><br /> -- OR -- <br /></td>
-    </tr>
-    <tr>
+      <td>Address: <input type="text" id="address" name="address" /> -- OR -- </td>
+      <td>City: <input type="text" id="city" name="city" /> -- OR -- </td>
       <td>Zip: <input type="text" id="zip" name="zip" /></td>
-    </tr>
-    <tr>
-    <td><input type="submit" value="View Location" /></td>
+      <td><input type="submit" value="View Location" /></td>
     </tr>
     </table>
   </form>
+  <br />
   <div id="map"></div>
   <script>
       var map;
-      function initMap() {
-      <?php
-        if(count($lat_lng) > 0){
-          echo 'var lat = '.$lat_lng[0].';';
-          echo 'var lng = '.$lat_lng[1].';';
+      var placesService;
+      var infoWindow;
+      function createMarkers(places){
+        var bounds = new google.maps.LatLngBounds();
+        var placesList = document.getElementById('places');
+
+        for (var i = 0, place; place = places[i]; i++) {
+          console.log('place # ' + i + ' : ' + place.name);
+          var image = {
+                        url: place.icon,
+                        size: new google.maps.Size(71, 71),
+                        origin: new google.maps.Point(0, 0),
+                        anchor: new google.maps.Point(17, 34),
+                        scaledSize: new google.maps.Size(25, 25)
+          };
+
+          var marker = new google.maps.Marker({
+                                  map: map,
+                                  icon: image,
+                                  title: place.name,
+                                  position: place.geometry.location
+                      });
+
+          var li = document.createElement('li');
+          li.textContent = place.name;
+          placesList.appendChild(li);
+
+          bounds.extend(place.geometry.location);
         }
-        else{
-          echo 'var lat = 40.397;';
-          echo 'var lng = -101.644;';
-        }
-      ?>
+        map.fitBounds(bounds);
+      }
+      
+      function initMap(){
+        <?php
+          if(count($lat_lng) > 0){
+            echo 'var lat = '.$lat_lng[0].';';
+            echo 'var lng = '.$lat_lng[1].';';
+          }
+          else{
+            echo 'var lat = 40.397;';
+            echo 'var lng = -101.644;';
+          }
+        ?>
         map = new google.maps.Map(document.getElementById('map'), {
           center: {
             lat: lat, 
             lng: lng
           },
-          zoom: 7
+          zoom: 11
         });
+        var request = {
+          query: 'Walmart',
+          fields: ['name', 'geometry'],
+          locationBias: {
+            lat: lat,
+            lng: lng,
+            radius: 5000
+          }
+        };
+        var placesService = new google.maps.places.PlacesService(map);
+        placesService.findPlaceFromQuery(request, function(results, status) {
+          if (status === google.maps.places.PlacesServiceStatus.OK) {
+            console.log('num results: ' + results.length);
+            createMarkers(results);
+            //for (var i = 0; i < results.length; i++) {
+              //createMarker(results[i]);
+            //}
+            map.setCenter(results[0].geometry.location);
+          }
+          else console.log('failed places status: ' + status);
+        });
+        
       }
+      
   </script>
-  <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyATdAW-nZvscm35rSLI8Bu9eGq84odzVLA&callback=initMap"
+  <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyATdAW-nZvscm35rSLI8Bu9eGq84odzVLA&callback=initMap&libraries=places,drawing"
     async defer></script>
+  <!-- <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyATdAW-nZvscm35rSLI8Bu9eGq84odzVLA&callback=initMap"
+    async defer></script> -->
     <!-- script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyATdAW-nZvscm35rSLI8Bu9eGq84odzVLA&libraries=places"
         async defer></script -->
   </body>
