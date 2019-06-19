b79facde19620f742063430eb04a2c113b6009ed
hrishikeshk <kv_hrishikesh@yahoo.com>
2019-06-19 17:54:52 +0530

    placeholder for app hits on brainyvestors and consequent redirects as required using PHP

---
+++ brainyvestors/app/index.php
@@ -0,0 +1,27 @@
+
+<?php
+/*
+if((!isset($_SERVER['HTTPS'])) || ($_SERVER['HTTPS'] != "on"))
+{
+	$url = "https://". $_SERVER['SERVER_NAME'] . $_SERVER['REQUEST_URI'];
+	header("Location: $url");
+	exit;
+}
+*/
+
+?>
+<!DOCTYPE html>
+<html>
+<head>
+<title>Brainyvestors</title>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta charset="utf-8">
+<meta name="keywords" content="Brainyvestors" />
+</head>
+<body>
+
+Site under construction. Coming soon !
+
+</body>
+</html>
+
